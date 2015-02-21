8645fe63d4cc189d539f07fc68cb96cc5cb3ce29
Boorawchick <Bezmerno@gmail.com>
2015-02-21 20:49:36 +0300

    can set random value

--- src/app/about/about.controller.js
+++ src/app/about/about.controller.js
@@ -2,5 +2,7 @@
 
 angular.module('myNewProject')
   .controller('AboutCtrl', function ($scope) {
-    $scope.awesomeThings = [];
+    $scope.newRandomValue = function() {
+      $scope.timer = (50 * Math.random()).toPrecision(2);
+    };
 });
